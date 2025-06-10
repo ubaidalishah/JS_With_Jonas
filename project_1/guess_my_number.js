c8b1cd7506a40a567b77ac647c594f1a2c9f4df5
ubaidalishah <ubaidalishah18@gmail.com>
2025-06-10 19:43:19 +0500

    refactor the code

--- project_1/guess_my_number.js
+++ project_1/guess_my_number.js
@@ -1,24 +1,24 @@
 const againBtn = document.querySelector("#again-btn");
 const middleBox = document.querySelector("#middle_box");
-
 const checkBtn = document.querySelector("#check_btn");
 let outputText = document.querySelector("#output_text");
-// let highScore = document.querySelector("#high_score");
-// console.log(checkBtn);
-let randomNumber = Math.floor(Math.random() * 20 + 1);
+let randomNumberGenerator = function () {
+  randomNumber = Math.floor(Math.random() * 20 + 1);
+  return randomNumber;
+};
+randomNumber = randomNumberGenerator();
 let score = 20;
 let highScore = 0;
-// document.querySelector("#high_score").textContent = highScore;
-checkBtn.addEventListener("click", function () {
-  let inputNumber = document.querySelector("#input_number");
-  inputNumber = parseInt(inputNumber.value);
-  console.log(inputNumber);
 
-  console.log(randomNumber);
+// game logic
+const gameLogicFunction = function () {
+  let inputNumber = Number(document.querySelector("#input_number").value);
+
   // when there is no input
   if (!inputNumber) {
     outputText.textContent = "No Number Enterd";
   }
+
   // when the guess is correct
   else if (inputNumber === randomNumber) {
     middleBox.textContent = inputNumber;
@@ -28,13 +28,13 @@ checkBtn.addEventListener("click", function () {
       highScore = score;
       document.querySelector("#high_score").textContent = highScore;
     }
+  }
 
-    // document.body.style.color = "Black";
-  }
-  // when the guess is high
-  else if (inputNumber > randomNumber) {
+  // when the guess is wrong
+  else if (inputNumber !== randomNumber) {
     if (score > 1) {
-      outputText.textContent = "The Guess is High";
+      outputText.textContent =
+        inputNumber > randomNumber ? "The Guess is High" : "The Guess is Low";
       score = score - 1;
       document.querySelector("#score").textContent = score;
     } else {
@@ -42,26 +42,21 @@ checkBtn.addEventListener("click", function () {
       document.querySelector("#score").textContent = 0;
     }
   }
-  // when the guess is low
-  else if (inputNumber < randomNumber) {
-    if (score > 1) {
-      outputText.textContent = "The Guess is Low";
-      score = score - 1;
-      document.querySelector("#score").textContent = score;
-    } else {
-      outputText.textContent = "You Lost";
-      document.querySelector("#score").textContent = 0;
-    }
-  }
-});
-// Again button functionality
-againBtn.addEventListener("click", () => {
+};
+
+// game reset
+const gameResetFunction = () => {
   outputText.textContent = "Start Guessing...";
   middleBox.textContent = "?";
   document.querySelector("#input_number").value = "";
   document.body.style.backgroundColor = "#1d293d ";
-  console.log("again btn is clicked");
   score = 20;
-  document.querySelector("#score").textContent = score;
-  randomNumber = Math.floor(Math.random() * 20 + 1);
-});
+  scoreTextContent = score;
+  randomNumber = randomNumberGenerator();
+};
+
+// check btn functionality
+checkBtn.addEventListener("click", gameLogicFunction);
+
+// Again button functionality
+againBtn.addEventListener("click", gameResetFunction);
